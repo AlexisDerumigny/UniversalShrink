5471e05348cbf6ff2493fabec449a201a09b4af3
Nestor Parolya <nestor@nparolya.tudelft.nl>
2026-01-23 11:16:16 +0100

    Documentation for gmv_moore_penrose plugin, gmv_moore_penrose eq target, gmv_moore_penrose geenral target

--- R/GMV_Moore_Penrose.R
+++ R/GMV_Moore_Penrose.R
@@ -3,6 +3,15 @@
 #' Optimal portfolio weights based on a plug-in of the Moore-Penrose inverse
 #' of the sample covariance matrix
 #' 
+#' Having the obervation matrix \eqn{\mathbf{Y}_n} and the corresponding sample 
+#' covariance matrix \eqn{\mathbf{S}_n} this estimator is given 
+#' by
+#' \deqn{
+#' \mathbf{w}_{MP}=\frac{\mathbf{S}^+_n\mathbf{1}}{\mathbf{1}^\top\mathbf{S}^+_n\mathbf{1}},
+#' }
+#' where \eqn{\mathbf{S}^+_n} is the Moore-Penrose (pseudo) inverse of 
+#'\eqn{\mathbf{S}_n} and and \eqn{\mathbf{1}} is a vector of ones.
+#' 
 #'
 #' @param Y data matrix (rows are features, columns are observations).
 #' TODO: transpose everything.
