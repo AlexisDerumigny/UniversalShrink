f399546a238017dc2280844d0d61c1c2ed57fe5f
AlexisDerumigny <AlexisDerumigny@users.noreply.github.com>
2026-02-26 10:47:46 +0100

    Improve documentation + add example for `GMV_Moore_Penrose`

--- R/GMV_Moore_Penrose.R
+++ R/GMV_Moore_Penrose.R
@@ -3,18 +3,41 @@
 #' Optimal portfolio weights based on a plug-in of the Moore-Penrose inverse
 #' of the sample covariance matrix
 #' 
-#' Having the obervation matrix \eqn{\mathbf{X}_n} and the corresponding sample 
-#' covariance matrix \eqn{\mathbf{S}_n} this estimator is given 
-#' by
+#' For an \eqn{n \times p} observation matrix \eqn{\mathbf{X}_n} and the
+#' corresponding sample covariance matrix \eqn{\mathbf{S}_n} of size
+#' \eqn{p \times p}, this estimator is given by
 #' \deqn{
 #' \mathbf{w}_{MP}=\frac{\mathbf{S}^+_n\mathbf{1}}{\mathbf{1}^\top\mathbf{S}^+_n\mathbf{1}},
 #' }
 #' where \eqn{\mathbf{S}^+_n} is the Moore-Penrose (pseudo) inverse of 
-#'\eqn{\mathbf{S}_n} and and \eqn{\mathbf{1}} is a vector of ones.
+#'\eqn{\mathbf{S}_n} and \eqn{\mathbf{1}} is a \eqn{p}-dimensional vector of ones.
 #' 
 #'
 #' @param X data matrix (rows are observations, columns are features).
 #' @inheritParams Moore_Penrose
+#' 
+#' @returns a vector of size \eqn{p} giving the optimal portfolio weights, with
+#' the elements summing up to \eqn{1}.
+#' 
+#' @seealso \code{\link{LossRelativeOutOfSampleVariance}} for computing the loss
+#' of the portfolio.
+#' 
+#' @examples
+#' Sigma = diag(1:5)
+#' 
+#' X <- MASS::mvrnorm(n = 100, mu = rep(0,5), Sigma = Sigma)
+#' weights100 = GMV_Moore_Penrose(X)
+#' 
+#' X <- MASS::mvrnorm(n = 3, mu = rep(0,5), Sigma = Sigma)
+#' weights3 = GMV_Moore_Penrose(X)
+#' 
+#' LossRelativeOutOfSampleVariance(weights100, Sigma)
+#' LossRelativeOutOfSampleVariance(weights3, Sigma)
+#' 
+#' trueWeights = rowSums(solve(Sigma)) / sum(solve(Sigma))
+#' 
+#' FrobeniusNorm2(trueWeights - weights100, normalized = FALSE)
+#' FrobeniusNorm2(trueWeights - weights3, normalized = FALSE)
 #' 
 #' @export
 GMV_Moore_Penrose <- function(X, centeredCov = TRUE)
