170606814f8e8717c9322a9d2931e027d73e80eb
AlexisDerumigny <AlexisDerumigny@users.noreply.github.com>
2026-03-04 18:06:01 +0100

    First version of all losses for precision matrix

---
+++ R/a_losses_all.R
@@ -0,0 +1,58 @@
+
+#' All quadratic losses
+#' 
+#' @examples
+#' 
+#' n = 100
+#' p = 200
+#' Sigma = diag(seq(1, 0.02, length.out = p))
+#' mu = rep(0, p)
+#' X <- MASS::mvrnorm(n = n, mu = mu, Sigma = Sigma)
+#' 
+#' precision_MP = Moore_Penrose(X)
+#' Losses(precision_MP, Sigma = Sigma)
+#' 
+#' @export
+Losses <- function(x, Sigma, ...) {
+  UseMethod("Losses")
+}
+
+#' @rdname Losses
+#' @export
+Losses.EstimatedPrecisionMatrix <- function(x, Sigma, SigmaInv = NULL, ...)
+{
+  if (is.null(SigmaInv)) {
+    SigmaInv = solve(Sigma)
+  }
+  Frob2 = c(LossFrobenius2(x = x, Sigma = Sigma, SigmaInv = SigmaInv,
+                           normalized = TRUE),
+            LossFrobenius2(x = x, Sigma = Sigma, SigmaInv = SigmaInv,
+                           normalized = FALSE))
+  
+  InvFrob2 = c(
+    LossInverseFrobenius2(x = x, Sigma = Sigma, SigmaInv = SigmaInv,
+                          normalized = TRUE),
+    LossInverseFrobenius2(x = x, Sigma = Sigma, SigmaInv = SigmaInv,
+                          normalized = FALSE))
+  
+  EuclEig = c(
+    LossEuclideanEigenvalues2(x = x, Sigma = Sigma, SigmaInv = SigmaInv,
+                              normalized = TRUE),
+    LossEuclideanEigenvalues2(x = x, Sigma = Sigma, SigmaInv = SigmaInv,
+                              normalized = FALSE))
+  
+  allLosses = rbind(
+    Frobenius2 = Frob2,
+    InverseFrobenius2 = InvFrob2,
+    EuclideanEigenvalues = EuclideanEigenvalues )
+  
+  allLosses = as.data.frame(allLosses)
+  colnames(allLosses) <- c("TRUE", "FALSE")
+  
+  result = list(
+    allLosses = allLosses
+  )
+  
+  return (result)
+}
+
