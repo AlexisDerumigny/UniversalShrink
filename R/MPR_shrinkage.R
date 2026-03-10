AlexisDerumigny/UniversalShrink

commit 3c363dda34b97b1a0d8a05016636020858983cb7

Fix incorrect renaming in 6ca59969ea045e82332487f2baef6e0cc7e1d0d6

R/MPR_shrinkage.R
@@ -127,7 +127,7 @@
 #' X <- MASS::mvrnorm(n = n, mu = mu, Sigma = Sigma)
 #' 
 #' # Estimation with default parameters (optimization) and identity target
-#' precision_MPR_optimal = MPR(X)
+#' precision_MPR_optimal = MPR_shrinkage(X)
 #' 
 #' cat("loss = ", LossFrobenius2(precision_MPR_optimal, Sigma = Sigma),
 #'     ", t opt = ", precision_MPR_optimal$t_optimal, 
@@ -136,7 +136,7 @@
 #' 
 #' # Estimation with default parameters (optimization) and oracle target
 #' oracle = solve(0.99 * Sigma + 0.01 * diag(nrow = p))
-#' precision_MPR_optimal_oracle = MPR(X, Pi0 = oracle)
+#' precision_MPR_optimal_oracle = MPR_shrinkage(X, Pi0 = oracle)
 #'   
 #' cat("loss = ", LossFrobenius2(precision_MPR_optimal_oracle, Sigma = Sigma),
 #'     ", t = ", precision_MPR_optimal_oracle$t_optimal, 
@@ -146,23 +146,23 @@
 #' # Trying suboptimal alpha and beta
 #' t_opt = precision_MPR_optimal$t_optimal
 #' 
-#' precision_MPR = MPR(X, t = t_opt, alpha = 1, beta = 0)
+#' precision_MPR = MPR_shrinkage(X, t = t_opt, alpha = 1, beta = 0)
 #' cat("loss = ", LossFrobenius2(precision_MPR, Sigma = Sigma))
 #' 
 #' # Trying suboptimal t, alpha and beta
-#' precision_MPR = MPR(X, t = 1, alpha = 1, beta = 0)
+#' precision_MPR = MPR_shrinkage(X, t = 1, alpha = 1, beta = 0)
 #'    
 #' cat("loss = ", LossFrobenius2(precision_MPR, Sigma = Sigma))
 #' 
 #' # Comparing with the non-shrinked version
-#' precision_MPR_no_shrink = MPR(X, t = t_opt)
+#' precision_MPR_no_shrink = MPR_no_shrinkage(X, t = t_opt)
 #'                                       
 #' cat("loss = ", LossFrobenius2(precision_MPR_no_shrink, Sigma = Sigma))
 #' 
 #' 
 #' @export
 #' 
-MPR <- function(X, centeredCov = TRUE, Pi0 = NULL,
+MPR_shrinkage <- function(X, centeredCov = TRUE, Pi0 = NULL,
                        t = NULL, alpha = NULL, beta = NULL,
                        verbose = 0,
                        eps = 1/(10^6), upp = pi/2 - eps, initialValue = 1.5)
@@ -179,15 +179,15 @@ MPR <- function(X, centeredCov = TRUE, Pi0 = NULL,
     result = switch(
       optimizationType,
       
-      none = MPR_identity(X = X, centeredCov = centeredCov,
+      none = MPR_shrinkage_identity(X = X, centeredCov = centeredCov,
                                  t = t, alpha = alpha, beta = beta,
                                  verbose = verbose, call_ = call_),
       
-      alpha_beta = MPR_identity_semioptimal(X = X, centeredCov = centeredCov,
+      alpha_beta = MPR_shrinkage_identity_semioptimal(X = X, centeredCov = centeredCov,
                                                    t = t, verbose = verbose,
                                                    call_ = call_),
       
-      all = MPR_identity_optimal(X = X, centeredCov = centeredCov,
+      all = MPR_shrinkage_identity_optimal(X = X, centeredCov = centeredCov,
                                         verbose = verbose, eps = eps, upp = upp,
                                         initialValue = initialValue, call_ = call_)
     )
@@ -196,15 +196,15 @@ MPR <- function(X, centeredCov = TRUE, Pi0 = NULL,
     result = switch(
       optimizationType,
       
-      none = MPR_general(X = X, centeredCov = centeredCov,
+      none = MPR_shrinkage_general(X = X, centeredCov = centeredCov,
                                 t = t, alpha = alpha, beta = beta, Pi0 = Pi0,
                                 verbose = verbose, call_ = call_),
       
-      alpha_beta = MPR_general_semioptimal(
+      alpha_beta = MPR_shrinkage_general_semioptimal(
         X = X, centeredCov = centeredCov, t = t, Pi0 = Pi0,
         verbose = verbose, call_ = call_),
       
-      all = MPR_general_optimal(X = X, centeredCov = centeredCov,
+      all = MPR_shrinkage_general_optimal(X = X, centeredCov = centeredCov,
                                        Pi0 = Pi0,
                                        verbose = verbose, eps = eps, upp = upp,
                                        initialValue = initialValue, call_ = call_)

man/MPR_shrinkage.Rd
@@ -1,0 +1,188 @@
+% Generated by roxygen2: do not edit by hand
+% Please edit documentation in R/MPR_shrinkage.R
+\name{MPR_shrinkage}
+\alias{MPR_shrinkage}
+\title{Moore-Penrose-Ridge shrinkage with general target}
+\usage{
+MPR_shrinkage(
+  X,
+  centeredCov = TRUE,
+  Pi0 = NULL,
+  t = NULL,
+  alpha = NULL,
+  beta = NULL,
+  verbose = 0,
+  eps = 1/(10^6),
+  upp = pi/2 - eps,
+  initialValue = 1.5
+)
+}
+\arguments{
+\item{X}{data matrix (rows are observations, columns are features).}
+
+\item{centeredCov}{Boolean variable. If \code{TRUE}, the covariance matrix is
+computed using centering (i.e. in the general case where the mean of the
+random vector may be non-zero).
+If \code{FALSE} the covariance matrix is computed assuming that the mean of
+the random vector of interest is \code{0} (i.e. does not need to be estimated).}
+
+\item{Pi0}{shrinkage target. By default it is the identity matrix and
+optimized computations are run in this case. Note that because of numerical
+issues (with rounding), specifying explicitly \code{Pi0 = diag(p)} may give
+different results for high values of \code{t}. This can arise as soon as
+\code{t = 10^4}.}
+
+\item{t, alpha, beta, eps, upp, initialValue}{\code{t}, \code{alpha} and
+\code{beta} are parameters of the estimation. In the optimized version,
+the loss is optimized with respect to \eqn{u = arctan(t)} over the interval
+\code{[eps, upp]}, and the optimizer starts at the \code{initialValue}.}
+
+\item{verbose}{a number indicating whether to print intermediary values and
+details about the progress of the computations. A value of \code{0} indicates
+no printing at all, while higher values indicate increasingly more detailed
+(more verbose) output.}
+}
+\value{
+the estimator of the precision matrix, of class
+\code{EstimatedPrecisionMatrix}.
+}
+\description{
+Following Bodnar and Parolya (2006), the shrinkage estimator
+for the precision matrix using the Moore-Penrose-Ridge inverse of the sample
+covariance matrix \eqn{\mathbf{S}_n} is computed as
+\deqn{
+\widehat{\boldsymbol{\Pi}}_{MPR}=\hat{\alpha}_{MPR}^*(t^*)\mathbf{S}_n^{\pm}(t^*)+
+\hat{\beta}_{MP}^*(t^*)\boldsymbol{\Pi}_0\,,
+} where \eqn{\mathbf{S}^+_n} denotes the Moore-Penrose inverse of the sample
+covariance matrix, \eqn{\boldsymbol{\Pi}_0} is the shrinkage target
+(by default, \eqn{\boldsymbol{\Pi}_0=\mathbf{I}_p}, i.e. shrinkage to the
+identity matrix) and \eqn{\hat{\alpha}_{MPR}^*(t^*)} and
+\eqn{\hat{\beta}_{MPR}^*(t^*)} are the optimal shrinkage intensities given by
+\deqn{
+\hat{\alpha}_{MPR}^*(t^*)
+=
+\dfrac{
+-\hat{v}^{(1)}(t^*)
+\hat{d}_1\left(t^*,\frac{1}{p}\boldsymbol{\Sigma}\right)
+\hat{q}_2\left(\frac{1}{p}\boldsymbol{\Pi}_0^2\right)
++
+\hat{v}^{(1)}(t^*)
+\hat{d}_1\left(t^*,\frac{1}{p}\boldsymbol{\Sigma}^2\boldsymbol{\Pi}_0\right)
+\hat{q}_1\left(\frac{1}{p}\boldsymbol{\Pi}_0\right)
+}{
+\hat{\grave{s}}_2\left(t^*,\frac{1}{p}\boldsymbol{\Sigma}^2\right)
+\hat{q}_2\left(\frac{1}{p}\boldsymbol{\Pi}_0^2\right)
+-
+[\hat{v}^{(1)}(t^*)]^2
+\hat{d}_1^2\left(t^*,\frac{1}{p}\boldsymbol{\Sigma}^2\boldsymbol{\Pi}_0\right)
+}
+} and
+\deqn{
+\hat{\beta}_{MPR}^*(t^*)
+=
+\dfrac{
+\hat{\grave{s}}_2\left(t^*,\frac{1}{p}\boldsymbol{\Sigma}^2\right)
+\hat{q}_1\left(\frac{1}{p}\boldsymbol{\Pi}_0\right)
+-
+[\hat{v}^{(1)}(t^*)]^2
+\hat{d}_1\left(t^*,\frac{1}{p}\boldsymbol{\Sigma}\right)
+\hat{d}_1\left(t^*,\frac{1}{p}\boldsymbol{\Sigma}^2\boldsymbol{\Pi}_0\right)
+}{
+\hat{\grave{s}}_2\left(t^*,\frac{1}{p}\boldsymbol{\Sigma}^2\right)
+\hat{q}_2\left(\frac{1}{p}\boldsymbol{\Pi}_0^2\right)
+-
+[\hat{v}^{(1)}(t^*)]^2
+\hat{d}_1^2\left(t^*,\frac{1}{p}\boldsymbol{\Sigma}^2\boldsymbol{\Pi}_0\right)
+}.
+}
+To determine the optimal value of the tuning parameter we
+maximize \eqn{\hat{L}^2_{MPR;2}(t)} over \eqn{(0,\infty)} and obtain \eqn{t^*}.
+The function \eqn{\hat{L}^2_{MPR;2}(t)} is given by
+\deqn{
+\hat{L}^2_{MPR;2}(t)
+=
+\dfrac{1}{\hat{q}_2\left(\frac{1}{p}\boldsymbol{\Pi}_0^2\right)}
+\dfrac{
+[\hat{v}^{(1)}(t)]^2
+\left[
+\hat{d}_1\left(t,\frac{1}{p}\boldsymbol{\Sigma}\right)
+\hat{q}_2\left(\frac{1}{p}\boldsymbol{\Pi}_0^2\right)
+-
+\hat{d}_1\left(t,\frac{1}{p}\boldsymbol{\Sigma}^2\boldsymbol{\Pi}_0\right)
+\hat{q}_1\left(\frac{1}{p}\boldsymbol{\Pi}_0\right)
+\right]^2
+}{
+\hat{\grave{s}}_2\left(t,\frac{1}{p}\boldsymbol{\Sigma}^2\right)
+\hat{q}_2\left(\frac{1}{p}\boldsymbol{\Pi}_0^2\right)
+-
+[\hat{v}^{(1)}(t)]^2
+\hat{d}_1^2\left(t,\frac{1}{p}\boldsymbol{\Sigma}^2\boldsymbol{\Pi}_0\right)
+}
+}
+where \eqn{\hat{v}^{(1)}(t)},
+\eqn{\hat{q}_1\left(\frac{1}{p}\boldsymbol{\Pi}_0\right)},
+\eqn{\hat{q}_2\left(\frac{1}{p}\boldsymbol{\Pi}_0^2\right)},
+\eqn{\hat{\grave{s}}_2\left(t,\frac{1}{p}\boldsymbol{\Sigma}^2\right)},
+\eqn{\hat{d}_1\left(t,\frac{1}{p}\boldsymbol{\Sigma}\right)}
+and
+\eqn{\hat{d}_1\left(t,\frac{1}{p}\boldsymbol{\Sigma}^2\boldsymbol{\Pi}_0\right)}
+are defined in the supplementary material of Bodnar and Parolya (2026).
+This procedure ensures that the loss
+\eqn{||\widehat{\boldsymbol{\Pi}}_{MPR}\boldsymbol{\Sigma}-\mathbf{I}_p||^2_F}
+is asymptotically minimized with probability one.
+}
+\examples{
+
+n = 10
+p = 5 * n
+mu = rep(0, p)
+
+# Generate Sigma
+X0 <- MASS::mvrnorm(n = 10*p, mu = mu, Sigma = diag(p))
+H <- eigen(t(X0) \%*\% X0)$vectors
+Sigma = H \%*\% diag(seq(1, 0.02, length.out = p)) \%*\% t(H)
+
+# Generate example dataset
+X <- MASS::mvrnorm(n = n, mu = mu, Sigma = Sigma)
+
+# Estimation with default parameters (optimization) and identity target
+precision_MPR_optimal = MPR_shrinkage(X)
+
+cat("loss = ", LossFrobenius2(precision_MPR_optimal, Sigma = Sigma),
+    ", t opt = ", precision_MPR_optimal$t_optimal, 
+    ", alpha opt = ", precision_MPR_optimal$alpha_optimal,
+    ", beta opt = ", precision_MPR_optimal$beta_optimal, "\n", sep = "")
+
+# Estimation with default parameters (optimization) and oracle target
+oracle = solve(0.99 * Sigma + 0.01 * diag(nrow = p))
+precision_MPR_optimal_oracle = MPR_shrinkage(X, Pi0 = oracle)
+  
+cat("loss = ", LossFrobenius2(precision_MPR_optimal_oracle, Sigma = Sigma),
+    ", t = ", precision_MPR_optimal_oracle$t_optimal, 
+    ", alpha opt = ", precision_MPR_optimal_oracle$alpha_optimal,
+    ", beta opt = ", precision_MPR_optimal_oracle$beta_optimal, "\n", sep = "")
+
+# Trying suboptimal alpha and beta
+t_opt = precision_MPR_optimal$t_optimal
+
+precision_MPR = MPR_shrinkage(X, t = t_opt, alpha = 1, beta = 0)
+cat("loss = ", LossFrobenius2(precision_MPR, Sigma = Sigma))
+
+# Trying suboptimal t, alpha and beta
+precision_MPR = MPR_shrinkage(X, t = 1, alpha = 1, beta = 0)
+   
+cat("loss = ", LossFrobenius2(precision_MPR, Sigma = Sigma))
+
+# Comparing with the non-shrinked version
+precision_MPR_no_shrink = MPR_no_shrinkage(X, t = t_opt)
+                                      
+cat("loss = ", LossFrobenius2(precision_MPR_no_shrink, Sigma = Sigma))
+
+
+}
+\references{
+Nestor Parolya & Taras Bodnar (2026).
+Reviving pseudo-inverses: Asymptotic properties of large dimensional
+Moore-Penrose and Ridge-type inverses with applications.
+\doi{10.1214/25-AOS2602}. ArXiv: \doi{10.48550/arXiv.2403.15792}.
+}

NAMESPACE
@@ -24,6 +24,7 @@ export(LossInverseFrobenius2)
 export(LossRelativeOutOfSampleVariance)
 export(Losses)
 export(MPR)
+export(MPR_shrinkage)
 export(Moore_Penrose)
 export(Moore_Penrose_higher_order_shrinkage)
 export(Moore_Penrose_shrinkage)

R/MPR_shrinkage_general.R
@@ -1,11 +1,11 @@
 
 
 
-MPR_general_optimal <- function (X, centeredCov, Pi0, verbose = 3, 
+MPR_shrinkage_general_optimal <- function (X, centeredCov, Pi0, verbose = 3, 
                                         eps = 1/(10^6), upp = pi/2 - eps,
                                         initialValue = 1.5, call_ = NULL){
   if (verbose > 0){
-    cat("Starting `MPR_general_optimal`...\n")
+    cat("Starting `MPR_shrinkage_general_optimal`...\n")
   }
   
   # Get sizes of X
@@ -24,7 +24,7 @@ MPR_general_optimal <- function (X, centeredCov, Pi0, verbose = 3,
     
     iS_ridge <- solve(S + t * Ip)
     
-    loss = loss_L2_MPR_optimal_target_general(
+    loss = loss_L2_MPR_optimal_shrinkage_general(
       t = t, Sn = S, p = p, Ip = Ip, cn = cn, Pi0 = Pi0, iS_ridge = iS_ridge, 
       verbose = verbose - 3)
     
@@ -66,10 +66,10 @@ MPR_general_optimal <- function (X, centeredCov, Pi0, verbose = 3,
   # This can also be written as:
   # iS_ridge %*% S %*% iS_ridge
   
-  MPR_general = alpha * MPR_estimator + beta * Pi0
+  MPR_shrinkage_general = alpha * MPR_estimator + beta * Pi0
   
   result = list(
-    estimated_precision_matrix = MPR_general,
+    estimated_precision_matrix = MPR_shrinkage_general,
     t_optimal = t,
     alpha_optimal = alpha,
     beta_optimal = beta,
@@ -98,7 +98,7 @@ MPR_general_optimal <- function (X, centeredCov, Pi0, verbose = 3,
 #' @returns an estimator of the L2 loss
 #'
 #' @noRd
-loss_L2_MPR_optimal_target_general <- function(t, Sn, p, Ip, cn, Pi0, iS_ridge, verbose)
+loss_L2_MPR_optimal_shrinkage_general <- function(t, Sn, p, Ip, cn, Pi0, iS_ridge, verbose)
 {
   hat_v_t0 = estimator_vhat_derivative(t = t, m = 0, iS_ridge = iS_ridge, p = p, 
                                        Ip = Ip, cn = cn)
@@ -140,7 +140,7 @@ loss_L2_MPR_optimal_target_general <- function(t, Sn, p, Ip, cn, Pi0, iS_ridge, 
 
 
 
-MPR_general_semioptimal <- function (X, centeredCov, t, Pi0, verbose = 2,
+MPR_shrinkage_general_semioptimal <- function (X, centeredCov, t, Pi0, verbose = 2,
                                             call_ = NULL){
   
   # Get sizes of X
@@ -168,10 +168,10 @@ MPR_general_semioptimal <- function (X, centeredCov, t, Pi0, verbose = 2,
   # This can also be written as:
   # iS_ridge %*% S %*% iS_ridge
   
-  MPR_general = alpha * MPR_estimator + beta * Pi0
+  MPR_shrinkage_general = alpha * MPR_estimator + beta * Pi0
   
   result = list(
-    estimated_precision_matrix = MPR_general,
+    estimated_precision_matrix = MPR_shrinkage_general,
     t = t,
     alpha_optimal = alpha,
     beta_optimal = beta,
@@ -461,7 +461,7 @@ best_alphabeta_MPR_shrinkage_general <- function(p, t0, cn, Pi0, Ip, Sn, iS_ridg
 
 
 
-MPR_general <- function (X, centeredCov, t, alpha, beta, Pi0, verbose,
+MPR_shrinkage_general <- function (X, centeredCov, t, alpha, beta, Pi0, verbose,
                                 call_ = NULL){
   
   # Get sizes of X
@@ -482,10 +482,10 @@ MPR_general <- function (X, centeredCov, t, alpha, beta, Pi0, verbose,
   # This can also be written as:
   # iS_ridge %*% S %*% iS_ridge
   
-  MPR_general = alpha * MPR_estimator + beta * Pi0
+  MPR_shrinkage_general = alpha * MPR_estimator + beta * Pi0
   
   result = list(
-    estimated_precision_matrix = MPR_general,
+    estimated_precision_matrix = MPR_shrinkage_general,
     t = t,
     alpha = alpha,
     beta = beta,

R/MPR_shrinkage_identity.R
@@ -1,10 +1,10 @@
 
 
-MPR_identity_optimal <- function (X, centeredCov = TRUE, verbose = 2,
+MPR_shrinkage_identity_optimal <- function (X, centeredCov = TRUE, verbose = 2,
                                          eps = 1/(10^6), upp = pi/2 - eps, 
                                          initialValue = 1.5, call_ = NULL){
   if (verbose > 0){
-    cat("Starting `MPR_identity_optimal`...\n")
+    cat("Starting `MPR_shrinkage_identity_optimal`...\n")
   }
   
   # Get sizes of X
@@ -50,10 +50,10 @@ MPR_identity_optimal <- function (X, centeredCov = TRUE, verbose = 2,
   # This can also be written as:
   # iS_ridge %*% S %*% iS_ridge
   
-  MPR_identity = alpha * MPR_estimator + beta * Ip
+  MPR_shrinkage_identity = alpha * MPR_estimator + beta * Ip
   
   result = list(
-    estimated_precision_matrix = MPR_identity,
+    estimated_precision_matrix = MPR_shrinkage_identity,
     t_optimal = t,
     alpha_optimal = alpha,
     beta_optimal = beta,
@@ -115,7 +115,7 @@ loss_L2_MPR_optimal <- function(t, S, cn, p, Ip){
 
 
 
-MPR_identity_semioptimal <- function (X, centeredCov = TRUE, t,
+MPR_shrinkage_identity_semioptimal <- function (X, centeredCov = TRUE, t,
                                              verbose = 2, call_ = NULL){
   
   # Get sizes of X
@@ -142,10 +142,10 @@ MPR_identity_semioptimal <- function (X, centeredCov = TRUE, t,
   # This can also be written as:
   # iS_ridge %*% S %*% iS_ridge
   
-  MPR_identity = alpha * MPR_estimator + beta * Ip
+  MPR_shrinkage_identity = alpha * MPR_estimator + beta * Ip
   
   result = list(
-    estimated_precision_matrix = MPR_identity,
+    estimated_precision_matrix = MPR_shrinkage_identity,
     t = t,
     alpha_optimal = alpha,
     beta_optimal = beta,
@@ -282,7 +282,7 @@ best_alphabeta_MPR_shrinkage_identity <- function(p, t, cn, S, iS_ridge, verbose
 }
 
 
-MPR_identity <- function (X, centeredCov = TRUE, t, alpha, beta,
+MPR_shrinkage_identity <- function (X, centeredCov = TRUE, t, alpha, beta,
                                  verbose = 0, call_ = NULL){
   
   # Get sizes of X
@@ -302,10 +302,10 @@ MPR_identity <- function (X, centeredCov = TRUE, t, alpha, beta,
   # This can also be written as:
   # iS_ridge %*% S %*% iS_ridge
   
-  MPR_identity = alpha * MPR_estimator + beta * Ip
+  MPR_shrinkage_identity = alpha * MPR_estimator + beta * Ip
   
   result = list(
-    estimated_precision_matrix = MPR_identity,
+    estimated_precision_matrix = MPR_shrinkage_identity,
     t = t,
     alpha = alpha,
     beta = beta,

man/MPR.Rd
@@ -1,34 +1,10 @@
 % Generated by roxygen2: do not edit by hand
-% Please edit documentation in R/MPR.R, R/MPR_shrinkage.R
+% Please edit documentation in R/MPR.R
 \name{MPR}
 \alias{MPR}
 \title{Moore-Penrose-Ridge}
 \usage{
-MPR(
-  X,
-  centeredCov = TRUE,
-  Pi0 = NULL,
-  t = NULL,
-  alpha = NULL,
-  beta = NULL,
-  verbose = 0,
-  eps = 1/(10^6),
-  upp = pi/2 - eps,
-  initialValue = 1.5
-)
-
-MPR(
-  X,
-  centeredCov = TRUE,
-  Pi0 = NULL,
-  t = NULL,
-  alpha = NULL,
-  beta = NULL,
-  verbose = 0,
-  eps = 1/(10^6),
-  upp = pi/2 - eps,
-  initialValue = 1.5
-)
+MPR(X, centeredCov = TRUE, t, verbose = 0)
 }
 \arguments{
 \item{X}{data matrix (rows are observations, columns are features).}
@@ -39,28 +15,14 @@ random vector may be non-zero).
 If \code{FALSE} the covariance matrix is computed assuming that the mean of
 the random vector of interest is \code{0} (i.e. does not need to be estimated).}
 
-\item{Pi0}{shrinkage target. By default it is the identity matrix and
-optimized computations are run in this case. Note that because of numerical
-issues (with rounding), specifying explicitly \code{Pi0 = diag(p)} may give
-different results for high values of \code{t}. This can arise as soon as
-\code{t = 10^4}.}
-
 \item{t}{parameter of the estimation.}
 
 \item{verbose}{a number indicating whether to print intermediary values and
 details about the progress of the computations. A value of \code{0} indicates
 no printing at all, while higher values indicate increasingly more detailed
 (more verbose) output.}
-
-\item{t, alpha, beta, eps, upp, initialValue}{\code{t}, \code{alpha} and
-\code{beta} are parameters of the estimation. In the optimized version,
-the loss is optimized with respect to \eqn{u = arctan(t)} over the interval
-\code{[eps, upp]}, and the optimizer starts at the \code{initialValue}.}
 }
 \value{
-the estimator of the precision matrix, of class
-\code{EstimatedPrecisionMatrix}.
-
 the estimator of the precision matrix, of class
 \code{EstimatedPrecisionMatrix}.
 }
@@ -77,90 +39,6 @@ t (\mathbf{S}_n + t \mathbf{I}_p)^{-2}
 where \eqn{\mathbf{S}_n} is the sample covariance matrix and \eqn{t>0}
 is a given parameter. By specifying \code{centeredCov=TRUE} (by default) one
 can choose between centered and noncentered versions of the sample covariance matrix.
-
-Following Bodnar and Parolya (2006), the shrinkage estimator
-for the precision matrix using the Moore-Penrose-Ridge inverse of the sample
-covariance matrix \eqn{\mathbf{S}_n} is computed as
-\deqn{
-\widehat{\boldsymbol{\Pi}}_{MPR}=\hat{\alpha}_{MPR}^*(t^*)\mathbf{S}_n^{\pm}(t^*)+
-\hat{\beta}_{MP}^*(t^*)\boldsymbol{\Pi}_0\,,
-} where \eqn{\mathbf{S}^+_n} denotes the Moore-Penrose inverse of the sample
-covariance matrix, \eqn{\boldsymbol{\Pi}_0} is the shrinkage target
-(by default, \eqn{\boldsymbol{\Pi}_0=\mathbf{I}_p}, i.e. shrinkage to the
-identity matrix) and \eqn{\hat{\alpha}_{MPR}^*(t^*)} and
-\eqn{\hat{\beta}_{MPR}^*(t^*)} are the optimal shrinkage intensities given by
-\deqn{
-\hat{\alpha}_{MPR}^*(t^*)
-=
-\dfrac{
--\hat{v}^{(1)}(t^*)
-\hat{d}_1\left(t^*,\frac{1}{p}\boldsymbol{\Sigma}\right)
-\hat{q}_2\left(\frac{1}{p}\boldsymbol{\Pi}_0^2\right)
-+
-\hat{v}^{(1)}(t^*)
-\hat{d}_1\left(t^*,\frac{1}{p}\boldsymbol{\Sigma}^2\boldsymbol{\Pi}_0\right)
-\hat{q}_1\left(\frac{1}{p}\boldsymbol{\Pi}_0\right)
-}{
-\hat{\grave{s}}_2\left(t^*,\frac{1}{p}\boldsymbol{\Sigma}^2\right)
-\hat{q}_2\left(\frac{1}{p}\boldsymbol{\Pi}_0^2\right)
--
-[\hat{v}^{(1)}(t^*)]^2
-\hat{d}_1^2\left(t^*,\frac{1}{p}\boldsymbol{\Sigma}^2\boldsymbol{\Pi}_0\right)
-}
-} and
-\deqn{
-\hat{\beta}_{MPR}^*(t^*)
-=
-\dfrac{
-\hat{\grave{s}}_2\left(t^*,\frac{1}{p}\boldsymbol{\Sigma}^2\right)
-\hat{q}_1\left(\frac{1}{p}\boldsymbol{\Pi}_0\right)
--
-[\hat{v}^{(1)}(t^*)]^2
-\hat{d}_1\left(t^*,\frac{1}{p}\boldsymbol{\Sigma}\right)
-\hat{d}_1\left(t^*,\frac{1}{p}\boldsymbol{\Sigma}^2\boldsymbol{\Pi}_0\right)
-}{
-\hat{\grave{s}}_2\left(t^*,\frac{1}{p}\boldsymbol{\Sigma}^2\right)
-\hat{q}_2\left(\frac{1}{p}\boldsymbol{\Pi}_0^2\right)
--
-[\hat{v}^{(1)}(t^*)]^2
-\hat{d}_1^2\left(t^*,\frac{1}{p}\boldsymbol{\Sigma}^2\boldsymbol{\Pi}_0\right)
-}.
-}
-To determine the optimal value of the tuning parameter we
-maximize \eqn{\hat{L}^2_{MPR;2}(t)} over \eqn{(0,\infty)} and obtain \eqn{t^*}.
-The function \eqn{\hat{L}^2_{MPR;2}(t)} is given by
-\deqn{
-\hat{L}^2_{MPR;2}(t)
-=
-\dfrac{1}{\hat{q}_2\left(\frac{1}{p}\boldsymbol{\Pi}_0^2\right)}
-\dfrac{
-[\hat{v}^{(1)}(t)]^2
-\left[
-\hat{d}_1\left(t,\frac{1}{p}\boldsymbol{\Sigma}\right)
-\hat{q}_2\left(\frac{1}{p}\boldsymbol{\Pi}_0^2\right)
--
-\hat{d}_1\left(t,\frac{1}{p}\boldsymbol{\Sigma}^2\boldsymbol{\Pi}_0\right)
-\hat{q}_1\left(\frac{1}{p}\boldsymbol{\Pi}_0\right)
-\right]^2
-}{
-\hat{\grave{s}}_2\left(t,\frac{1}{p}\boldsymbol{\Sigma}^2\right)
-\hat{q}_2\left(\frac{1}{p}\boldsymbol{\Pi}_0^2\right)
--
-[\hat{v}^{(1)}(t)]^2
-\hat{d}_1^2\left(t,\frac{1}{p}\boldsymbol{\Sigma}^2\boldsymbol{\Pi}_0\right)
-}
-}
-where \eqn{\hat{v}^{(1)}(t)},
-\eqn{\hat{q}_1\left(\frac{1}{p}\boldsymbol{\Pi}_0\right)},
-\eqn{\hat{q}_2\left(\frac{1}{p}\boldsymbol{\Pi}_0^2\right)},
-\eqn{\hat{\grave{s}}_2\left(t,\frac{1}{p}\boldsymbol{\Sigma}^2\right)},
-\eqn{\hat{d}_1\left(t,\frac{1}{p}\boldsymbol{\Sigma}\right)}
-and
-\eqn{\hat{d}_1\left(t,\frac{1}{p}\boldsymbol{\Sigma}^2\boldsymbol{\Pi}_0\right)}
-are defined in the supplementary material of Bodnar and Parolya (2026).
-This procedure ensures that the loss
-\eqn{||\widehat{\boldsymbol{\Pi}}_{MPR}\boldsymbol{\Sigma}-\mathbf{I}_p||^2_F}
-is asymptotically minimized with probability one.
 }
 \examples{
 
@@ -198,60 +76,8 @@ for (t in c(0.01, 0.2, 0.5, 1, 2)){
 }
 
 
-
-n = 10
-p = 5 * n
-mu = rep(0, p)
-
-# Generate Sigma
-X0 <- MASS::mvrnorm(n = 10*p, mu = mu, Sigma = diag(p))
-H <- eigen(t(X0) \%*\% X0)$vectors
-Sigma = H \%*\% diag(seq(1, 0.02, length.out = p)) \%*\% t(H)
-
-# Generate example dataset
-X <- MASS::mvrnorm(n = n, mu = mu, Sigma = Sigma)
-
-# Estimation with default parameters (optimization) and identity target
-precision_MPR_optimal = MPR(X)
-
-cat("loss = ", LossFrobenius2(precision_MPR_optimal, Sigma = Sigma),
-    ", t opt = ", precision_MPR_optimal$t_optimal, 
-    ", alpha opt = ", precision_MPR_optimal$alpha_optimal,
-    ", beta opt = ", precision_MPR_optimal$beta_optimal, "\n", sep = "")
-
-# Estimation with default parameters (optimization) and oracle target
-oracle = solve(0.99 * Sigma + 0.01 * diag(nrow = p))
-precision_MPR_optimal_oracle = MPR(X, Pi0 = oracle)
-  
-cat("loss = ", LossFrobenius2(precision_MPR_optimal_oracle, Sigma = Sigma),
-    ", t = ", precision_MPR_optimal_oracle$t_optimal, 
-    ", alpha opt = ", precision_MPR_optimal_oracle$alpha_optimal,
-    ", beta opt = ", precision_MPR_optimal_oracle$beta_optimal, "\n", sep = "")
-
-# Trying suboptimal alpha and beta
-t_opt = precision_MPR_optimal$t_optimal
-
-precision_MPR = MPR(X, t = t_opt, alpha = 1, beta = 0)
-cat("loss = ", LossFrobenius2(precision_MPR, Sigma = Sigma))
-
-# Trying suboptimal t, alpha and beta
-precision_MPR = MPR(X, t = 1, alpha = 1, beta = 0)
-   
-cat("loss = ", LossFrobenius2(precision_MPR, Sigma = Sigma))
-
-# Comparing with the non-shrinked version
-precision_MPR_no_shrink = MPR(X, t = t_opt)
-                                      
-cat("loss = ", LossFrobenius2(precision_MPR_no_shrink, Sigma = Sigma))
-
-
 }
 \references{
-Nestor Parolya & Taras Bodnar (2026).
-Reviving pseudo-inverses: Asymptotic properties of large dimensional
-Moore-Penrose and Ridge-type inverses with applications.
-\doi{10.1214/25-AOS2602}. ArXiv: \doi{10.48550/arXiv.2403.15792}.
-
 Nestor Parolya & Taras Bodnar (2026).
 Reviving pseudo-inverses: Asymptotic properties of large dimensional
 Moore-Penrose and Ridge-type inverses with applications.

commit 6bb9e5d0331a9f9a8fe8173303e0d4aa92af777b

Fix bug in example of `MPR_shrinkage` introduced by 3c363dda34b97b1a0d8a05016636020858983cb7

R/MPR_shrinkage.R
@@ -155,7 +155,7 @@
 #' cat("loss = ", LossFrobenius2(precision_MPR, Sigma = Sigma))
 #' 
 #' # Comparing with the non-shrinked version
-#' precision_MPR_no_shrink = MPR_no_shrinkage(X, t = t_opt)
+#' precision_MPR_no_shrink = MPR(X, t = t_opt)
 #'                                       
 #' cat("loss = ", LossFrobenius2(precision_MPR_no_shrink, Sigma = Sigma))
 #' 

man/MPR_shrinkage.Rd
@@ -174,7 +174,7 @@ precision_MPR = MPR_shrinkage(X, t = 1, alpha = 1, beta = 0)
 cat("loss = ", LossFrobenius2(precision_MPR, Sigma = Sigma))
 
 # Comparing with the non-shrinked version
-precision_MPR_no_shrink = MPR_no_shrinkage(X, t = t_opt)
+precision_MPR_no_shrink = MPR(X, t = t_opt)
                                       
 cat("loss = ", LossFrobenius2(precision_MPR_no_shrink, Sigma = Sigma))
 