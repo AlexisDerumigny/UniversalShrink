fb61b7360bc2c4d8a9f9e860d0c84517e6317753
AlexisDerumigny <AlexisDerumigny@users.noreply.github.com>
2026-04-22 10:42:55 +0200

    Convert `b` to numeric class in `prepare_and_check_b`

--- R/GMV-utils.R
+++ R/GMV-utils.R
@@ -15,7 +15,18 @@ prepare_and_check_b <- function(b, p, tolerance = 0.001)
 {
   if (is.null(b)){
     b = rep(1/p, length = p)
-  } else if (length(b) != p){
+    
+    return (b)
+  }
+  
+  # Now we know that `b` is non-NULL, i.e. entered by the user.
+  # so we try to convert it to a numeric vector.
+  b = as.numeric(b)
+  if (anyNA(b)){
+    UniversalShrink_warning_condition_base(
+      paste0(length(which(is.na(b))), " NAs in target portfolio 'b'."))
+  }
+  if (length(b) != p){
     stop("'b' should be a vector of length 'p'.")
   }
   if (abs(sum(b) - 1) > tolerance){
