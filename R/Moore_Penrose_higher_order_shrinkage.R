2e32d5f34e8a8fc256b81c14e0b0d6af1e0caab3
AlexisDerumigny <AlexisDerumigny@users.noreply.github.com>
2026-06-08 11:15:59 +0200

    Use the C++ Bell polynomials in `compute_M_MoorePenrose_plarge`

--- R/Moore_Penrose_higher_order_shrinkage.R
+++ R/Moore_Penrose_higher_order_shrinkage.R
@@ -100,15 +100,18 @@ compute_M_MoorePenrose_plarge <- function(
   
   v <- estimator_vhat_derivative_t0(m = 2 * m, c_n, p = p, D_MP = D_MP)
   
-  Bell_polynomials = matrix(nrow = 2 * m, ncol = 2 * m)
-  for (j in 1:(2*m))
-  {
-    for (k in 1:j)
-    {
-      Bell_polynomials[j, k] = 
-        kStatistics::e_eBellPol(j, k, c(v[1:(j - k + 1)], rep(0, k - 1)))
-    }
-  }
+  # Bell_polynomials = matrix(nrow = 2 * m, ncol = 2 * m)
+  # for (j in 1:(2*m))
+  # {
+  #   for (k in 1:j)
+  #   {
+  #     Bell_polynomials[j, k] = 
+  #       kStatistics::e_eBellPol(j, k, c(v[1:(j - k + 1)], rep(0, k - 1)))
+  #   }
+  # }
+  Bell_polynomials = bellPolynomials(v, verbose = 0)
+  # Removing the lines corresponding to n = 0 and k = 0
+  Bell_polynomials = Bell_polynomials[-1, -1]
   
   h <- rep(NA, 2*m+1)
   h[2] <- h2
@@ -219,8 +222,8 @@ compute_M_MoorePenrose_plarge <- function(
     for (k in 1:j)
     {
       hm[j+1] <- hm[j+1] +
-        (-1)^(j + k + 1) * factorial(k) / factorial(j) * d[k,1] *
-        kStatistics::e_eBellPol(j, k, c(v[1:(j - k + 1)], rep(0, k - 1)))
+        (-1)^(j + k + 1) * factorial(k) / factorial(j) * d[k,1] * 
+        Bell_polynomials[j, k]
     }
   }
   hm <- Re(hm)
