22240271e2728cdced7dd0b47ab07be52d3bedf3
AlexisDerumigny <AlexisDerumigny@users.noreply.github.com>
2026-07-20 16:19:40 +0200

    Create `optimization` function as a wrapper
    to decide whether to do smoothed optimization or not

--- R/smoothed_optimization.R
+++ R/smoothed_optimization.R
@@ -1,7 +1,22 @@
 
+optimization <- function(FUN, smoothed, grid = NULL, k = NULL, verbose, ...)
+{
+  if (smoothed) {
+    result = smoothed_optimization(
+      FUN = FUN, grid = grid, k = k, verbose = verbose, ...)
+  }
+  
+  return (result)
+}
+
 
 smoothed_optimization <- function(FUN, grid, k, verbose, ...)
 {
+  if (verbose > 0) {
+    cat("Starting `smoothed_optimization` ... \n")
+    cat("k = ", k, "\n")
+  }
+  
   n_t = length(grid)
   vec_loss = rep(NA_real_, length(grid))
   for (i_t in 1:length(grid)) {
@@ -16,7 +31,11 @@ smoothed_optimization <- function(FUN, grid, k, verbose, ...)
   
   result = list(
     optimal_t = optimal_t,
-    optimal_t_smoothed = optimal_t_smoothed
+    optimal_t_smoothed = optimal_t_smoothed,
+    optimization_type = "smoothed",
+    k = k,
+    grid = grid,
+    vec_loss = vec_loss
   )
   
   return (result)
